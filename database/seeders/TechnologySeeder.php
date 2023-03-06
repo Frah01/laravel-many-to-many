ef71e45a6ce11d3d919dc90fb22f13a97e0030ff
Frah01 <vinario.francesco@gmail.com>
2023-03-06 14:03:13 +0100

    Fatta tabella pivot

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Technology;
 
 class TechnologySeeder extends Seeder
 {
@@ -14,6 +15,12 @@ class TechnologySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $technologies = ['HTML/CSS', 'JavaScript', 'Vuejs', 'PHP', 'SQL', 'Laravel'];
+        foreach ($technologies as $technology) {
+            $newTechnology = new Technology();
+            $newTechnology->name = $technology;
+            $newTechnology->slug = Technology::generateSlug($newTechnology->name);
+            $newTechnology->save();
+        }
     }
 }
